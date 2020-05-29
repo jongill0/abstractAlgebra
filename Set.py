e986200f450e20a0e9e1e643ed044156b856e7d7
Jon Gill <jon.gill@berkeley.edu>
2020-05-29 14:03:01 -0700

    Added function

--- Set.py
+++ Set.py
@@ -1,13 +1,33 @@
-import numpy as np
 import random
 
-class Set(_set):
+class Set:
+
     def __init__(self, _set):
         _set = set(_set)
         self.set = _set
 
+    def __repr__(self):
+        return str({a for a in self.items()})
+
+    def __len__(self):
+        return len(self.items())
+
+    def items(self):
+        return self.set
+
     def cartesian_product(self, other):
-        return Set((a, b) for a in self.set for b in other)
+        if not isinstance(other, Set):
+            other = Set(other)
+        return Set((a, b) for a in self.items() for b in other.items())
 
     def pick_arbitrary(self):
-        return random.choice(self.set)
+        return random.choice(self.items())
+
+    def is_empty(self):
+        return len(self.items()) == 0
+
+    def is_subset(self, other):
+        for a in self.items():
+            if a not in other.items():
+                return False
+        return True
